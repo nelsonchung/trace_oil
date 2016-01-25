303ef8b798f08cf11d520e47951abc2162ca9202
nelsonchung <chihchun.chung@gmail.com>
2016-01-25 17:43:36 +0800

    data.txt record the information of oil including 淨值 市價 and 原油價格

--- oil.py
+++ oil.py
@@ -2,6 +2,8 @@
 
 import requests
 from pyquery import PyQuery as pq
+#from yahoo_finance import Share
+from grs import Stock
 
 url_oil="http://www.plus500.com/zh/Instruments/CL"
 
@@ -13,3 +15,15 @@ q = pq(url_oil)
 print "Oil Now: "+(q('#ctl00_ContentPlaceMain1_LabelBuyPriceBig').text())
 print "Oil High:"+(q('#ctl00_ContentPlaceMain1_LabelHighPriceValue').text())
 print "Oil Low: "+(q('#ctl00_ContentPlaceMain1_LabelLowPriceValue').text())
+
+
+
+#yahoo = Share('YHOO')
+#print yahoo.get_open()
+#print yahoo.get_price()
+#print yahoo.get_trade_datetime()
+#yahoo.refresh()
+
+stock = Stock('00642U')
+print stock.moving_average(5)
+
