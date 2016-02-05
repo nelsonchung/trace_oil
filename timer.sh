c0b8c89da5c2ecd34a4294b3f9b6f683a57ed231
nelsonchung <chihchun.chung@gmail.com>
2016-02-05 11:09:57 +0800

    Modify the sync time. Original setting of HOUR_RUN_ETF 14 seems not workable.

--- timer.sh
+++ timer.sh
@@ -1,7 +1,8 @@
 DATE=`date +"%Y-%m-%d"`
-HOUR_RUN_ETF=14
+#HOUR_RUN_ETF=14
+HOUR_RUN_ETF=06
 MINUTE_RUN_ETF=00
-HOUR_RUN_OIL=09
+HOUR_RUN_OIL=06
 MINUTE_RUN_OIL=00
 #echo $DATE
 
@@ -10,15 +11,19 @@ do
     HOUR=`date +"%H"`
     MINUTE=`date +"%M"`
     DAY_OF_WEEK=`date +"%u"`
+    echo "HOUR is $HOUR"
+    echo "MINUTE is $MINUTE"
     #echo "Now time is $HOUR:$MINUTE $DAY_OF_WEEK"
     #echo "Alert time is $HOUR_RUN:$MINUTE_RUN "
 
     #if [ "$DAY_OF_WEEK" == "1" ] || [ "$DAY_OF_WEEK" == "2" ] || [ "$DAY_OF_WEEK" == "3" ] || [ "$DAY_OF_WEEK" == "4" ] || [ "$DAY_OF_WEEK" == "5" ] ; then
         if [ "$HOUR" == "$HOUR_RUN_ETF" ] && [ "$MINUTE" == "$MINUTE_RUN_ETF" ]; then
             etf_value=`python get_etf.py`
+            echo $etf_value
         fi
         if [ "$HOUR" == "$HOUR_RUN_OIL" ] && [ "$MINUTE" == "$MINUTE_RUN_OIL" ]; then
             oil_value=`python get_oil.py`
+            echo $oil_value
             sed '2i '"${etf_value}  ${oil_value}" -i data.txt
         fi
     #fi
