b5b1b220bf0ebd9ce72e2f7f5d616ca474b6ccf3
nelsonchung <chihchun.chung@gmail.com>
2016-05-03 10:52:08 +0800

    remove update2thingspeak.py

--- update2thingspeak.py
+++ update2thingspeak.py
@@ -2,8 +2,8 @@ import httplib, urllib
 from time import localtime, strftime
 import time
 
-def doit():
-    params = urllib.urlencode({'field1': worth_value, 'field2': market_value, 'field3': oil_price, 'key':'9P9ETYQIZPAW0948'})
+def updateetf(worth_value, market_value):
+    params = urllib.urlencode({'field1': worth_value, 'field2': market_value, 'key':'9P9ETYQIZPAW0948'})
 	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 	conn = httplib.HTTPConnection("api.thingspeak.com:80")
 	
@@ -12,6 +12,21 @@ def doit():
 		response = conn.getresponse()
 		print worth_value
 		print market_value
+		print strftime("%a, %d %b %Y %H:%M:%S", localtime())
+		print response.status, response.reason
+		data = response.read()
+		conn.close()
+	except:
+		print "connection failed"	
+
+def updateoilprice(oil_price):
+    params = urllib.urlencode({'field3': oil_price, 'key':'9P9ETYQIZPAW0948'})
+	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+	conn = httplib.HTTPConnection("api.thingspeak.com:80")
+	
+	try:
+		conn.request("POST", "/update", params, headers)
+		response = conn.getresponse()
 		print oil_price 
 		print strftime("%a, %d %b %Y %H:%M:%S", localtime())
 		print response.status, response.reason
@@ -19,6 +34,7 @@ def doit():
 		conn.close()
 	except:
 		print "connection failed"	
+
 
 #sleep for 16 seconds (api limit of 15 secs)
 if __name__ == "__main__":
