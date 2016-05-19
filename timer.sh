a462125fc6d20a7f1d97a8b18cd58d5e39b3ed87
nelsonchung <chihchun.chung@gmail.com>
2016-05-19 16:27:30 +0800

    加入觀察元黃金的腳本

--- timer.sh
+++ timer.sh
@@ -22,11 +22,16 @@ do
         if [ "$HOUR" == "$HOUR_RUN_ETF" ] && [ "$MINUTE" == "$MINUTE_RUN_ETF" ]; then
             etf_value=`python get_etf.py`
             echo $etf_value
+            #for golden
+            etf_golden_value=`python get_etf_golden.py`
+            echo $etf_golden_value
         fi
         if [ "$HOUR" == "$HOUR_RUN_OIL" ] && [ "$MINUTE" == "$MINUTE_RUN_OIL" ]; then
             oil_value=`python get_oil.py`
             echo $oil_value
             sed '2i '"${etf_value}  ${oil_value}" -i data.txt
+            #for golden
+            sed '2i '"${etf_golden_value}" -i data_golden.txt
         fi
         if [ "$HOUR" == "$HOUR_COMMIT" ] && [ "$MINUTE" == "$MINUTE_COMMIT" ]; then
             echo "Run git commit and git push"
