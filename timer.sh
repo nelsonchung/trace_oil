0a5cbe2c1de7bea35c6141e83ffbaf4d7a3e210c
nelsonchung <chihchun.chung@gmail.com>
2016-07-27 09:14:22 +0800

    Update commit format

--- timer.sh
+++ timer.sh
@@ -32,11 +32,13 @@ do
             sed '2i '"${etf_value}  ${oil_value}" -i data.txt
             #for golden
             sed '2i '"${etf_golden_value}" -i data_golden.txt
+            commit_log="Update golden information - 日期 淨值 市值: ${etf_golden_value} "
+            git commit data_golden.txt -m "${commit_log}"
         fi
         if [ "$HOUR" == "$HOUR_COMMIT" ] && [ "$MINUTE" == "$MINUTE_COMMIT" ]; then
             echo "Run git commit and git push"
             commit_log="Update oil information - 日期 淨值 市值 原油價格: ${etf_value} ${oil_value}"
-            git commit data.txt -m ${commit_log} 
+            git commit data.txt -m "${commit_log}"
             git push
         fi
     #fi
